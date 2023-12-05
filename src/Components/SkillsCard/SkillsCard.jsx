14d42046da4b31471507907e5250b9db9ad04040
mahalteja <mahaltejapilla08@gmail.com>
2023-12-05 13:05:58 +0530

    skills

--- src/Components/SkillsCard/SkillsCard.jsx
+++ src/Components/SkillsCard/SkillsCard.jsx
@@ -1,24 +1,49 @@
 import React, { useEffect } from "react";
 import "./SkillsCard.css";
 import { ui } from "../Assets/data";
+import { frontend } from "../Assets/data";
+import { others } from "../Assets/data";
 import Item from "../Items/Item";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 const SkillsCard = () => {
-    useEffect(()=>{
-        AOS.init({duration:2000})
-    },[])
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
   return (
-    <div className="card">
-      <div className="card-heading">
-        <h1 data-aos="fade-up">UI & UX Designing</h1>
+    <div className="cards" data-aos="fade-up">
+      <div id="card">
+        <div id="card-heading">
+          <h1>{ui.nam}</h1>
+        </div>
+        <div id="card-items">
+          {ui.skill.map((item, i) => {
+            return <Item {...item} />;
+          })}
+        </div>
       </div>
-      <div className="card-items" data-aos="fade-up">
-        {ui.map((item, i) => {
-          return <Item {...item} />;
-        })}
+      <div id="card">
+        <div id="card-heading">
+          <h1>{frontend.nam}</h1>
+        </div>
+        <div id="card-items">
+          {frontend.skill.map((item, i) => {
+            return <Item {...item} />;
+          })}
+        </div>
       </div>
+      <div id="card">
+        <div id="card-heading">
+          <h1>{others.nam}</h1>
+        </div>
+        <div id="card-items">
+          {others.skill.map((item, i) => {
+            return <Item {...item} />;
+          })}
+        </div>
+      </div>
+      
     </div>
   );
 };
